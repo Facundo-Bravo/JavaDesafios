43e789cfcce329d64f90706efa4fee98046847b5
Facundo Bravo <facundombravo@gmail.com>
2022-07-12 21:36:37 -0300

    correccion git commit -m 'correccion'

--- main.js
+++ main.js
@@ -6,17 +6,24 @@ let nota3 = parseInt (prompt('ingrese la tercer nota'))
 let ListaDeNotas = [nota1,nota2,nota3]
 
 function ContadorDeNotas (ListaDeNotas){
-    let SumadorDeNotas = 0;
+    let ContadorDeNotas=0;
     for(let i = 0; i < 3; i++){
-        SumadorDeNotas = SumadorDeNotas + ListaDeNotas[i];
+        ContadorDeNotas = ContadorDeNotas + ListaDeNotas[i];
     }
-    return SumadorDeNotas;
+    return ContadorDeNotas;
 }
 
-const promedio = SumadorDeNotas/3;
-if(promedio > 6){
-    console.log('Aprobaste la cursada, tu nota es: ' + promedio)
+let contador = ContadorDeNotas(ListaDeNotas);
+
+function SacarPromedio(contador){
+    let promedio = Math.round(contador/3);
+    return promedio;
+}
+
+let PromedioFinal = SacarPromedio(contador)
+if(PromedioFinal > 6){
+    alert('Tu nota es de ' + PromedioFinal + 'Estás Aprobado')
 } else {
-    console.log('Reprobaste la cursada , tu nota es: ' + promedio)
+    alert('Tu nota es de ' + PromedioFinal + 'Estás Desaprobado')
 }
 
