e11f41e70c09a01594d3fb1a6bbd3db635714aa9
Facundo Bravo <facundombravo@gmail.com>
2022-07-12 21:19:59 -0300

    Primera entrega final

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-let precio1 = parseInt (prompt('ingrese precio del primer producto'))
-let precio2 = parseInt (prompt('ingrese precio del segundo producto'))
-let precio3 = parseInt (prompt('ingrese precio del tercer producto'))
 
-const ListaDePrecios = [precio1,precio2,precio3]
-let SumadorDePrecios = 0;
+let nota1 = parseInt (prompt('ingrese la primer nota'))
+let nota2 = parseInt (prompt('ingrese la segunda nota'))
+let nota3 = parseInt (prompt('ingrese la tercer nota'))
 
-for(let i = 0; i < 3; i++){
-    SumadorDePrecios = SumadorDePrecios + ListaDePrecios[i];
+let ListaDeNotas = [nota1,nota2,nota3]
+
+function ContadorDeNotas (ListaDeNotas){
+    let SumadorDeNotas = 0;
+    for(let i = 0; i < 3; i++){
+        SumadorDeNotas = SumadorDeNotas + ListaDeNotas[i];
+    }
+    return SumadorDeNotas;
 }
 
-console.log('Tu monto a pagar es ' + SumadorDePrecios);
+const promedio = SumadorDeNotas/3;
+if(promedio > 6){
+    console.log('Aprobaste la cursada, tu nota es: ' + promedio)
+} else {
+    console.log('Reprobaste la cursada , tu nota es: ' + promedio)
+}
+
