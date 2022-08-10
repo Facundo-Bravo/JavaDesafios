dfb02da4fe702250d00c6cfd26123c217fea8246
Facundo Bravo <facundombravo@gmail.com>
2022-08-09 22:15:59 -0300

    Segunda entrega Proyecto Final

--- main.js
+++ main.js
@@ -1,24 +1,70 @@
-// DESAFIO EVENTOS//
-const carrito = [];
+// SEGUNDA ENTREGA PROYECTO FINAL//
+const carrito = JSON.parse(localStorage.getItem("carrito")) ?? [];
+const total = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+document.getElementById("cart-total").innerHTML = `${carrito.length} - $${total}`;
+
+function generarCardsCarrito() { 
+    carrito.forEach((producto)=>{
+        document.getElementById("cards-modal").innerHTML += `<div>
+        <p>${producto.id} 
+        - ${producto.nombre}
+        - ${producto.precio}
+      </div>`
+    })
+}
+
 
 const productos = [
     {id:1000, nombre: "Puas", precio: 100},
     {id:1001, nombre: "Cable", precio: 2300},
     {id:1002, nombre: "Correa", precio: 1650},
+    {id:1003, nombre: "Afinador", precio: 3587},
 ];
 
 let cards = "";
 productos.forEach((producto) =>{
-    cards += `<div class='card d-flex col-4'>
-    <h3>${producto.nombre}</h3>
-    <button onclick='agregarAlCarrito("${producto.nombre}")'>COMPRAR</button>
-    </div>`
+    const idButton = `add-cart${producto.id}`
+    document.getElementById("seccion-card").innerHTML += `<div class="col mb-5">
+    <div class="card h-100">
+        <!-- Product image-->
+        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+        <!-- Product details-->
+        <div class="card-body p-4">
+            <div class="text-center">
+                <!-- Product name-->
+                <h5 class="fw-bolder">${producto.nombre}</h5>
+                <!-- Product price-->
+               $ ${producto.precio}
+            </div>
+        </div>
+        <!-- Product actions-->
+        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center"><a id= "${idButton}" data-id="${producto.id}" class="btn btn-outline-dark mt-auto" href="#">COMPRAR</a></div>
+        </div>
+    </div>
+</div>`;
 })
 
-function agregarAlCarrito(tituloProducto){
-    alert("Agregaste: "+tituloProducto)
-}
-document.write (cards);
+productos.forEach((producto) => {
+    const idButton = `add-cart${producto.id}`
+    document.getElementById(idButton).addEventListener('click',(event) => {
+        carrito.push(producto);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+        const total = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+        document.getElementById("cart-total").innerHTML = `${carrito.length} - $${total}`;
+        document.getElementById("cart-elements").innerHTML = ""
+        carrito.forEach((producto)=>{
+            document.getElementById("cart-elements").innerHTML += `<tr>
+            <th scope="row">${producto.id} </th>
+            <td>${producto.nombre} </td>
+            <td>${producto.precio} </td>
+          </tr>`
+        })
+    })
+});
+
+
+
 
 
 
